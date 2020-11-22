1dd0b30694186f24f91148f06c3cd182b35551ef
Sunny <ousunny91@gmail.com>
2020-11-22 14:13:54 -0500

    Setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const indexRoutes = require('./routes/index');
+
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/public`));
+
+app.use('/', indexRoutes);
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});

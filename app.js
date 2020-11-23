b5daf19286da1b2040211cfa851bfb8c3a24dea3
Sunny <ousunny91@gmail.com>
2020-11-22 20:42:48 -0500

    Setup

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const indexRoutes = require('./routes/index');
 
 const port = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/', indexRoutes);

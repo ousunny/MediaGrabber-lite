13599d6ce484764df682b0087078c11c2a86f792
Sunny <ousunny91@gmail.com>
2020-11-23 13:55:12 -0500

    Add video and mp3 download

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
 const indexRoutes = require('./routes/index');
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
 
 router.get('/', (req, res) => {
     res.render('index');
+});
+
+router.get('/download', async (req, res, next) => {
+    console.log(req.query);
+
+    const { url, type } = req.query;
+
+    let title = 'title';
+
+    await ytdl.getBasicInfo(url, { format: 'mp4' }).then((info) => {
+        title = encodeURI(info.videoDetails.title);
+    });
+
+    if (type === 'video') {
+        res.header('Content-Disposition', `attachment; filename=${title}.mp4`);
+        ytdl(url, { format: 'mp4' }).pipe(res);
+    } else if (type === 'mp3') {
+        const audio = ytdl(url, {
+            filter: 'audioonly',
+            quality: 'highestaudio',
+        });
+
+        res.header('Content-Disposition', `attachment; filename=${title}.mp3`);
+
+        ffmpeg(audio)
+            .audioCodec('libmp3lame')
+            .audioBitrate(128)
+            .format('mp3')
+            .on('error', (err) => console.log(err))
+            .on('end', () => console.log('Finished'))
+            .pipe(res, { end: true });
+    }
 });
 
 module.exports = router;
